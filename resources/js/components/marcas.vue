5a8223d9a4d03e1cf8fb1edf91260d44f0f2dc0a
FlavioCandidoSilva <flaviocandidopolicarpo@hotmail.com>
2023-06-20 13:48:01 -0300

    criando modal interativo

---
+++ resources/js/components/marcas.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <card-component titulo="Busca de marcas">
+                    <template v-slot:conteudo>
+                        <div class="row">
+                            <div class="mb-3 col">
+                                <input-container-component titulo="ID" id="inputId" id-help="idHelp"
+                                    texto-ajuda="Informe o ID da marca">
+                                    <input type="number" placeholder="ID" class="form-control" id="inputId"
+                                        aria-describedby="idHelp">
+                                </input-container-component>
+                            </div>
+                            <div class="mb-3 col">
+                                <input-container-component titulo="Nome da marca" id="inputNome" id-help="nomeHelp"
+                                    texto-ajuda="Informe o nome da marca">
+                                    <input type="text" placeholder="Nome da marca" class="form-control" id="inputNome"
+                                        aria-describedby="nomeHelp">
+                                </input-container-component>
+                            </div>
+                        </div>
+                    </template>
+                    <template v-slot:rodape>
+                        <button type="submit" class="btn btn-primary btn-sm float-right">Pesquisar</button>
+                    </template>
+                </card-component>
+                <!-- card marca -->
+
+                <card-component titulo="Relação de marca">
+                    <template v-slot:conteudo>
+                        <table-component></table-component>
+                    </template>
+                    <template v-slot:rodape>
+                        <button type="button" class="btn btn-primary btn-sm float-right" data-bs-toggle="modal"
+                            data-bs-target="#modalMarca">Adicionar</button>
+                    </template>
+                </card-component>
+            </div>
+        </div>
+        <modal-component id="modalMarca" titulo="Adicionar marca">
+            <template v-slot:conteudo>
+                <div class="form-group">
+                    <input-container-component titulo="Nome da marca" id="novoNome" id-help="novoNomeHelp"
+                        texto-ajuda="Informe o nome da marca">
+                        <input type="text" placeholder="Nome da marca" class="form-control" id="novoNome"
+                            aria-describedby="novoNomeHelp">
+                    </input-container-component>
+                </div>
+
+                <div class="form-group">
+                    <input-container-component titulo="Imagem" id="novoImagem" id-help="novoImagemHelp"
+                        texto-ajuda="Informe o nome da marca">
+                        <input type="file" placeholder="Selecione uma imagem" class="form-control-file" id="novoImagem"
+                            aria-describedby="novoImagemHelp">
+                    </input-container-component>
+                </div>
+            </template>
+            <template v-slot:rodape>
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+                <button type="button" class="btn btn-primary">Salvar</button>
+            </template>
+        </modal-component>
+
+    </div>
+</template>
+
+<script>
+</script>
